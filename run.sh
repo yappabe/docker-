51a418690ebd785905f46fa7627dcca54407347c
Joeri Verdeyen <joeriv@yappa.be>
2015-06-03 15:35:15 +0200

    Added #!/bin/bash to run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sed -i "s|DOCUMENT_ROOT|$DOCUMENT_ROOT|g" /etc/apache2/sites-available/000-default.conf && \
 sed -i "s|PHP_FPM_SOCKET|$PHP_FPM_SOCKET|g" /etc/apache2/sites-available/000-default.conf && \
 /usr/sbin/apache2ctl -D FOREGROUND
